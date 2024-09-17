abd7d5f735d33cafda3e746a8ff6636e45e5768d
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-17 11:56:02 -0300

    questões 5 e 6 finalizadas

--- src/implementacao/5-maior-valor-digitado.ts
+++ src/implementacao/5-maior-valor-digitado.ts
@@ -1,3 +1,10 @@
-function mostrarMaiorValor() {
-    return null;
+function mostrarMaiorValor(numeros: number[]){
+    let maior = 0;
+    for(let i = 0; i <= numeros.length; i++){
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
+    }
+    console.log(maior);
 }
+mostrarMaiorValor([4,2,1,9]);
--- src/implementacao/6-segundo-maior-valor.ts
+++ src/implementacao/6-segundo-maior-valor.ts
@@ -1,3 +1,13 @@
-function mostrarSegundoMaiorValor() {
-    return null;
+//Faça um programa que leia N números e mostre o segundo maior valor digitado.
+function mostrarSegundoMaiorValor(numeros: number[]) {
+    let maior = 0;
+    let segundoMaior = 0;
+    for(let i = 0; i <= numeros.length; i++){
+        if(numeros[i] > maior){
+            segundoMaior = maior;
+            maior = numeros[i];
+        }
+    }
+    console.log(segundoMaior);
 }
+mostrarSegundoMaiorValor([4,8,1,12]);
